8eb54592d4a1c376abb43198261ef4582fa0bf7d
Sourav Mondal <souravm@capitalnumbers.com>
2015-07-22 15:10:51 +0530

    first commit with plugin info file

---
+++ wpWikiTags.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Wp Wiki tags
+Plugin URI: http://souravmondal.co.in/about
+Description: This plugin is aimed to convert all abbreviation tags within a WordPress post or page into corresponding Wikipedia links.
+Author: Sourav Mondal
+Version: 1.0
+Author URI: http://souravmondal.co.in/about
+*/
